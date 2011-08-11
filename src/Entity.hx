268e413570cf81dc320c06c14c1a6d2b3f73dc66
Nathan Ostgard <noonat@phuce.com>
2011-08-10 20:04:46 -0700

    Added Entity class

---
+++ src/Entity.hx
@@ -0,0 +1,148 @@
+package;
+
+private typedef WorldFriend = {
+  function addToNames(entity:Entity):Void;
+  function removeFromNames(entity:Entity):Void;
+  function addToTags(entity:Entity, tag:String):Void;
+  function removeFromTags(entity:Entity, tag:String):Void;
+};
+
+class Entity {
+  public var flags:Int;
+  public var isActive:Bool;
+  public var isVisible:Bool;
+  public var name(getName, setName):String;
+  public var world:World;
+  public var x(getX, setX):Float;
+  public var y(getY, setY):Float;
+
+  var _name:String;
+  var _tags:Hash<Bool>;
+  var _x:Float;
+  var _y:Float;
+
+  public function new() {
+    flags = 0;
+    isActive = true;
+    isVisible = true;
+    world = null;
+    _name = null;
+    _tags = new Hash<Bool>();
+    _x = 0;
+    _y = 0;
+  }
+
+  public function added() {
+    
+  }
+
+  public function removed() {
+    
+  }
+
+  public function update() {
+    
+  }
+
+  inline public function addTag(tag:String) {
+    if (!hasTag(tag)) {
+      _tags.set(tag, true);
+      if (world != null) {
+        getWorldFriend().addToTags(this, tag);
+      }
+    }
+  }
+
+  inline public function getTags():Iterator<String> {
+    return _tags.keys();
+  }
+
+  inline public function hasTag(tag:String):Bool {
+    return _tags.exists(tag);
+  }
+
+  inline public function removeTag(tag:String) {
+    if (world != null) {
+      if (_tags.exists(tag)) {
+        getWorldFriend().removeFromTags(this, tag);
+      }
+    }
+    _tags.remove(tag);
+  }
+
+  inline public function removeAllTags() {
+    if (world != null) {
+      for (tag in getTags()) {
+        getWorldFriend().removeFromTags(this, tag);
+        _tags.remove(tag);
+      }
+    } else {
+      for (tag in getTags()) {
+        _tags.remove(tag);
+      }
+    }
+  }
+
+  inline public function addFlags(mask:Int) {
+    flags |= mask;
+  }
+
+  inline public function removeFlags(mask:Int) {
+    flags &= ~mask;
+  }
+
+  inline public function hasFlags(mask:Int):Bool {
+    return flags & mask == mask;
+  }
+
+  inline public function hasAnyFlags(mask:Int):Bool {
+    return flags & mask != 0;
+  }
+
+  inline function getName():String {
+    return _name;
+  }
+
+  inline function setName(value:String):String {
+    if (world != null) {
+      if (_name != null) {
+        getWorldFriend().removeFromNames(this);
+      }
+      _name = value;
+      if (_name != null) {
+        getWorldFriend().addToNames(this);
+      }
+    } else {
+      _name = value;
+    }
+    return value;
+  }
+
+  inline function getX():Float {
+    return _x;
+  }
+
+  inline function setX(value:Float):Float {
+    return _x = value;
+  }
+
+  inline function getY():Float {
+    return _y;
+  }
+
+  inline function setY(value:Float):Float {
+    return _y = value;
+  }
+
+  inline function getWorldFriend():WorldFriend {
+    return untyped world;
+  }
+
+  static public var listPool:ValueListPool<Entity>;
+  static public var nodePool:ValueNodePool<Entity>;
+
+  static public function __init__() {
+    nodePool = new ValueNodePool<Entity>();
+    listPool = new ValueListPool<Entity>(nodePool);
+  }
+}
--- src/World.hx
+++ src/World.hx
@@ -3,18 +3,57 @@ package;
 class World {
   public var isActive:Bool;
   public var isVisible:Bool;
+  var _entities:ValueList<Entity>;
+  var _entitiesToAdd:ValueList<Entity>;
+  var _entitiesToRemove:ValueList<Entity>;
+  var _names:Hash<ValueList<Entity>>;
+  var _tags:Hash<ValueList<Entity>>;
 
   public function new() {
     isActive = true;
     isVisible = true;
+    _entities = Entity.listPool.create();
+    _entitiesToAdd = Entity.listPool.create();
+    _entitiesToRemove = Entity.listPool.create();
+    _names = new Hash<ValueList<Entity>>();
+    _tags = new Hash<ValueList<Entity>>();
   }
 
   public function begin() {
-
+    
   }
 
   public function end() {
+    removeAll();
+    resolvePending();
+  }
 
+  /**
+  * Add an entity to the world. The entity will not be added until the end of
+  * the current frame.
+  */
+  public function add(entity:Entity) {
+    _entitiesToAdd.add(entity);
+  }
+
+  /**
+  * Remove an entity from the world. The entity will not be removed until the
+  * end of the current frame.
+  */
+  public function remove(entity:Entity) {
+    _entitiesToRemove.add(entity);
+  }
+
+  /**
+  * Remove all entities from the world at the end of the frame.
+  */
+  public function removeAll() {
+    _entitiesToRemove.clear();
+    var node = _entities.first;
+    while (node != null) {
+      _entitiesToRemove.add(node.value);
+      node = node.next;
+    }
   }
 
   /**
@@ -27,6 +66,7 @@ class World {
     if (isVisible) {
       render();
     }
+    resolvePending();
   }
 
   /**
@@ -40,6 +80,94 @@ class World {
   * Update all the active entities in the world.
   */
   public function update() {
+    var node = _entities.first;
+    while (node != null) {
+      var entity = node.value;
+      node = node.next;
+      if (entity.isActive) {
+        entity.update();
+      }
+    }
+  }
 
+  function resolvePending() {
+    resolvePendingRemoves();
+    resolvePendingAdds();
+  }
+
+  function resolvePendingAdds() {
+    while (_entitiesToAdd.first != null) {
+      var entity = _entitiesToAdd.shift();
+      if (entity == null) {
+        continue;
+      } else if (entity.world == null) {
+        _entities.add(entity);
+        entity.world = this;
+        addToNames(entity);
+        for (tag in entity.getTags()) {
+          addToTags(entity, tag);
+        }
+        entity.added();
+      }
+    }
+  }
+
+  function resolvePendingRemoves() {
+    while (_entitiesToRemove.first != null) {
+      var entity = _entitiesToRemove.shift();
+      if (entity == null) {
+        continue;
+      } else if (entity.world == null) {
+        _entitiesToAdd.remove(entity);
+      } else if (entity.world == this) {
+        entity.removed();
+        for (tag in entity.getTags()) {
+          removeFromTags(entity, tag);
+        }
+        removeFromNames(entity);
+        entity.world = null;
+        _entities.remove(entity);
+      }
+    }
+  }
+
+  function addToNames(entity:Entity) {
+    var entities = _names.get(entity.name);
+    if (entities == null) {
+      entities = Entity.listPool.create();
+      _names.set(entity.name, entities);
+    }
+    entities.add(entity);
+  }
+
+  function removeFromNames(entity:Entity) {
+    var entities = _names.get(entity.name);
+    if (entities != null) {
+      entities.remove(entity);
+      if (entities.first == null) {
+        entities.free();
+        _names.remove(entity.name);
+      }
+    }
+  }
+
+  function addToTags(entity:Entity, tag:String) {
+    var entities = _tags.get(tag);
+    if (entities == null) {
+      entities = Entity.listPool.create();
+      _tags.set(tag, entities);
+    }
+    entities.add(entity);
+  }
+
+  function removeFromTags(entity:Entity, tag:String) {
+    var entities = _tags.get(tag);
+    if (entities != null) {
+      entities.remove(entity);
+      if (entities.first == null) {
+        entities.free();
+        _tags.remove(tag);
+      }
+    }
   }
 }
