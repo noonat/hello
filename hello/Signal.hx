a4a406146b9c69c8f100a240a5cc4bccf2c98d95
Nathan Ostgard <noonat@phuce.com>
2011-08-24 21:11:56 -0700

    Cleaned up Signal code

--- hello/Signal.hx
+++ hello/Signal.hx
@@ -3,7 +3,7 @@ package hello;
 /**
  * A signal class, inspired by Robert Penner's as3-signals, but lighter
  * weight for use with FlashPunk.
- * 
+ *
  *    var onHurt:Signal = new Signal();
  *    onHurt.add(function(damage:Number) {
  *      trace(damage, 'damage?! ouch!');
@@ -11,7 +11,8 @@ package hello;
  *    onHurt.dispatch(42);  // calls the listener
  */
 class Signal<T> {
-  var _dispatching:Bool;
+  public var hasListeners(getHasListeners, never):Bool;
+  var _isDispatching:Bool;
   var _listenerHead:SignalListener;
   var _listenerTail:SignalListener;
   var _removed:Array<SignalListener>;
@@ -20,10 +21,10 @@ class Signal<T> {
    * Constructor.
    */
   public function new() {
-    _dispatching = false;
+    _isDispatching = false;
     _listenerHead = null;
     _listenerTail = null;
-    _removed = null;
+    _removed = [];
   }
 
   /**
@@ -32,17 +33,17 @@ class Signal<T> {
    */
   inline public function add(listener:T) {
     if (find(listener) == null) {
-      var sl:SignalListener = new SignalListener();
-      sl.added = true;
-      sl.listener = listener;
-      sl.prev = _listenerTail;
-      sl.next = null;
+      var node = new SignalListener();
+      node.isActive = true;
+      node.listener = listener;
+      node.prev = _listenerTail;
+      node.next = null;
       if (_listenerHead == null) {
-        _listenerHead = sl;
-        _listenerTail = sl;
+        _listenerHead = node;
+        _listenerTail = node;
       } else {
-        _listenerTail.next = sl;
-        _listenerTail = sl;
+        _listenerTail.next = node;
+        _listenerTail = node;
       }
     }
   }
@@ -53,14 +54,14 @@ class Signal<T> {
    * @return Matching SignalListener, or null if the function wasn't a listener on this signal.
    */
   inline public function find(listener:T):SignalListener {
-    var sl:SignalListener = _listenerHead;
-    while (sl != null) {
-      if (sl.listener == listener) {
+    var node = _listenerHead;
+    while (node != null) {
+      if (node.listener == listener) {
         break;
       }
-      sl = sl.next;
+      node = node.next;
     }
-    return sl;
+    return node;
   }
 
   /**
@@ -68,9 +69,9 @@ class Signal<T> {
    * @param listener Function to remove.
    */
   inline public function remove(listener:T) {
-    var sl:SignalListener = find(listener);
-    if (sl != null) {
-      _remove(sl);
+    var node = find(listener);
+    if (node != null) {
+      removeNode(node);
     }
   }
 
@@ -78,11 +79,11 @@ class Signal<T> {
    * Remove all listeners from this signal.
    */
   inline public function removeAll() {
-    var sl:SignalListener = _listenerHead;
-    while (sl != null) {
-      var sln:SignalListener = sl.next;
-      _remove(sl);
-      sl = sln;
+    var node = _listenerHead;
+    while (node != null) {
+      var next = node.next;
+      removeNode(node);
+      node = next;
     }
     _listenerHead = _listenerTail = null;
   }
@@ -91,50 +92,57 @@ class Signal<T> {
    * Trigger this signal, invoking all of the listeners in the order they were added.
    * @param args Arguments to pass along to the listeners.
    */
-  public function dispatch(args:Array<Dynamic> = null) {
-    _dispatching = true;
-    var sl:SignalListener = _listenerHead;
+  public function dispatch(args:Array<Dynamic>=null) {
+    removeMarkedNodes();
+    _isDispatching = true;
     // This is split up for performance
-    var numArgs:Int = args == null ? 0 : args.length;
+    var numArgs = args == null ? 0 : args.length;
     if (numArgs == 0) {
-      while (sl != null) {
-        if (sl.added) {
-          sl.listener();
-        }
-        sl = sl.next;
-      }
+      dispatch0();
     } else if (numArgs == 1) {
-      var arg:Dynamic = args[0];
-      while (sl != null) {
-        if (sl.added) {
-          sl.listener(arg);
-        }
-        sl = sl.next;
-      }
+      dispatch1(args[0]);
     } else if (numArgs == 2) {
-      var arg1:Dynamic = args[0];
-      var arg2:Dynamic = args[1];
-      while (sl != null) {
-        if (sl.added) {
-          sl.listener(arg1, arg2);
-        }
-        sl = sl.next;
-      }
+      dispatch2(args[0], args[1]);
     } else {
-      while (sl != null) {
-        if (sl.added) {
-          Reflect.callMethod(null, sl.listener, args);
+      var node = _listenerHead;
+      while (node != null) {
+        if (node.isActive) {
+          Reflect.callMethod(null, node.listener, args);
         }
-        sl = sl.next;
+        node = node.next;
       }
     }
-    _dispatching = false;
-    if (_removed != null && _removed.length > 0) {
-      var i:Int = _removed.length;
-      while (i-- > 0) {
-        _remove(_removed[i]);
+    _isDispatching = false;
+    removeMarkedNodes();
+  }
+
+  inline public function dispatch0() {
+    var node = _listenerHead;
+    while (node != null) {
+      if (node.isActive) {
+        node.listener();
       }
-      _removed = null;
+      node = node.next;
+    }
+  }
+
+  inline public function dispatch1(arg:Dynamic) {
+    var node = _listenerHead;
+    while (node != null) {
+      if (node.isActive) {
+        node.listener(arg);
+      }
+      node = node.next;
+    }
+  }
+
+  inline public function dispatch2(arg1:Dynamic, arg2:Dynamic) {
+    var node = _listenerHead;
+    while (node != null) {
+      if (node.isActive) {
+        node.listener(arg1, arg2);
+      }
+      node = node.next;
     }
   }
 
@@ -142,32 +150,45 @@ class Signal<T> {
    * Remove a signal listener, or mark it for removal if a dispatch is in progress.
    * @param sl Signal listener to remove from the linked list.
    */
-  inline function _remove(sl:SignalListener) {
-    if (sl != null) {
-      if (_dispatching && sl.added) {
-        if (_removed == null) {
-          _removed = [sl];
-        } else {
-          _removed[_removed.length] = sl;
-        }
-        sl.added = false;
+  inline function removeNode(node:SignalListener) {
+    if (node != null) {
+      if (_isDispatching && node.isActive) {
+        _removed[_removed.length] = node;
+        node.isActive = false;
       } else {
-        if (sl == _listenerHead) {
-          _listenerHead = sl.next;
+        if (node == _listenerHead) {
+          _listenerHead = node.next;
         }
-        if (sl == _listenerTail) {
-          _listenerTail = sl.prev;
+        if (node == _listenerTail) {
+          _listenerTail = node.prev;
         }
-        if (sl.prev != null) {
-          sl.prev.next = sl.next;
+        if (node.prev != null) {
+          node.prev.next = node.next;
         }
-        if (sl.next != null) {
-          sl.next.prev = sl.prev;
+        if (node.next != null) {
+          node.next.prev = node.prev;
         }
-        sl.next = sl.prev = null;
-        sl.listener = null;
-        sl.added = false;
+        node.next = null;
+        node.prev = null;
+        node.listener = null;
+        node.isActive = false;
       }
     }
+  }
+
+  /**
+  * Remove any nodes that were previously marked for removal.
+  */
+  inline function removeMarkedNodes() {
+    if (_removed.length > 0) {
+      var node:SignalListener;
+      while ((node = _removed.pop()) != null) {
+        removeNode(node);
+      }
+    }
+  }
+
+  inline function getHasListeners():Bool {
+    return _listenerHead != null;
   }
 }
--- hello/SignalListener.hx
+++ hello/SignalListener.hx
@@ -1,13 +1,13 @@
 package hello;
 
 class SignalListener {
-  public var added:Bool;
+  public var isActive:Bool;
   public var listener:Dynamic;
   public var next:SignalListener;
   public var prev:SignalListener;
 
   public function new() {
-    added = true;
+    isActive = true;
     listener = null;
     next = null;
     prev = null;
